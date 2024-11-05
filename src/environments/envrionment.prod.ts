18b2c144e248732bbb82f3db69488d7a8d68258b
Pablo Conejos <126771457+pabloconejos@users.noreply.github.com>
2024-11-05 21:54:32 +0100

    Delete src/environments/envrionment.prod.ts

--- src/environments/envrionment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: true, // Cambiar a true para entornos de producción
-    apiUrl: 'http://localhost:3000', // URL de la API para el entorno de prueba
-};
